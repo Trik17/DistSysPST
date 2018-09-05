737e7bda3930f5a77d180384ca0cd692c9f6bde7
Trik17 <andreatricarico17@gmail.com>
2018-09-05 08:57:40 -0500

    niente

--- src/main/java/it/polimi/dist/ServerPackage/TimerThread.java
+++ src/main/java/it/polimi/dist/ServerPackage/TimerThread.java
@@ -11,7 +11,7 @@ public class TimerThread extends Thread {
     private Server server;
     private Message messageToResend;
     private int toSleep = 5000;
-    private int retransmissionThreshold = 6;
+    private int retransmissionThreshold = 8;
 
 
     public TimerThread(Message message, Server server) {
